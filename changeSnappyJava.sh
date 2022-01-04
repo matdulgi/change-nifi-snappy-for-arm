bbdd0dc72b946d8937da5c3d9deea6395535074c
leedulgi <yrdog9494@gmail.com>
2022-01-04 09:10:20 +0900

    add sudo, run sh on not only chsj directory

--- changeSnappyJava.sh
+++ changeSnappyJava.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 teeCmd="tee changeSnappyJava.log"
+username=hdfs
+nifiHome=/usr/local/nifi
+chsjHome=$nifiHome/changeSnappyJava
 echo start changeSnappyJava.sh | $teeCmd
 date >> changeSnappyJava.log
+sudo=sudo
 
 
 snappyFileArr=($(find /usr/local/nifi/work -name "snappy*"))
@@ -13,15 +17,16 @@ for oldFile in ${snappyFileArr[@]};do
 
 #backup old snappy
     echo "backup old snappy : $oldFile -> snappyJava_bkup" | $teeCmd
-    cp $oldFile snappyJava_bkup
+    $sudo chown $username:$username $oldFile
+    cp $oldFile $chsjHome/snappyJava_bkup
 
 #remove old snappy-java
-    echo remove : $path >> changeSnappyJava.log | $teeCmd
-    rm $oldFile
+    echo remove : $path >> ${chsjHome}/changeSnappyJava.log | $teeCmd
+    $sudo rm $oldFile
 
 #copy new snappy-java to pathes
     echo "copy new snappy-java : $path -> changeSnappyJava.log" | $teeCmd
-    cp snappy-java-1.1.8.2.jar $path
+    $sudo cp ${chsjHome}/snappy-java-1.1.8.2.jar $path
 done
 
 #echo "$pathArr" | $teeCmd 
